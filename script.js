dea39350b310d7f0508a2e0ef10b615a1a5ec804
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-04-28 10:41:50 -0400

    final clean-up

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ if (computerChoice < 0.34) {
 } console.log("Computer: " + computerChoice);
 
 var compare = function (choice1, choice2) {
-if (choice1 === choice2) {
-    return "The result is a tie!";
-}
+    if (choice1 === choice2) {
+        return "The result is a tie!";
+    }
     else if (choice1 === "rock") {
-    if (choice2 === "scissors") {
-        return "rock wins";
+        if (choice2 === "scissors") {
+            return "rock wins";
+        }
+        else {
+            return "paper wins";
+        }
     }
-    else {
-        return "paper wins";
-    }
-}
     else if (choice1 === "paper") {
         if (choice2 === "rock") {
             return "paper wins!";
@@ -31,11 +31,11 @@ if (choice1 === choice2) {
     
     else if (choice1 === "scissors") {
         if (choice2 === "paper") {
-        return "scissors wins";
-    }
+            return "scissors wins";
+        }
         else {
             return "rock wins";
-    }
+        }
     }
     
     else  {
