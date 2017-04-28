c578c35eac07bb3d26f50c7304df0eaf92292133
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-04-28 09:44:26 -0400

    add 3 computer choices

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
+
+if (computerChoice <= 0.33) {
+    computerChoice = "rock";
+}
+
+else if (computerChoice >=0.34 && computerChoice <=0.66) {
+    computerChoice = "paper";
+}
+
+else {
+    console.log("scissors");
+}
