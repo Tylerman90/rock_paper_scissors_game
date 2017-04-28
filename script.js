152d0e2e6c903d6f74c51e56d796045e5550d94f
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-04-28 10:38:08 -0400

    add return value for invalid entries

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
-	computerChoice = "rock";
+    computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
+    computerChoice = "paper";
 } else {
-	computerChoice = "scissors";
+    computerChoice = "scissors";
 } console.log("Computer: " + computerChoice);
 
 var compare = function (choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    }
+if (choice1 === choice2) {
+    return "The result is a tie!";
+}
     else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            return "rock wins";
-        }
-        else {
-            return "paper wins";
-        }
+    if (choice2 === "scissors") {
+        return "rock wins";
     }
+    else {
+        return "paper wins";
+    }
+}
     else if (choice1 === "paper") {
         if (choice2 === "rock") {
             return "paper wins!";
@@ -31,11 +31,15 @@ var compare = function (choice1, choice2) {
     
     else if (choice1 === "scissors") {
         if (choice2 === "paper") {
-            return "scissors wins";
-        }
+        return "scissors wins";
+    }
         else {
             return "rock wins";
-        }
+    }
+    }
+    
+    else  {
+        return "entry invalid";
     }
 };
 
