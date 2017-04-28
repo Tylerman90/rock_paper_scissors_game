156045bf6e2d057402937a793a5817844cd67019
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-04-28 10:09:59 -0400

    add rock scenario

--- script.js
+++ script.js
@@ -14,7 +14,15 @@ else {
 }*/
 
 var compare = function compare (choice1, choice2) {
-if (choice1 === choice2) {
-    return "The result is a tie!";
-}
+	if (choice1 === choice2) {
+		return "The result is a tie!";
+	}
+	else if (choice1 === "rock") {
+		if (choice2 === "scissors") {
+			return "rock wins";
+		}
+		else {
+			return "paper wins";
+		}
+	}
 };
